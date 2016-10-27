df7b9476b81e5a7938e7c702983dad89bad1e57b
Alvaro Niño Pinto <anino@unac.edu.co>
2016-10-27 13:27:45 -0500

    Add project files.

---
+++ MVC5RealWorld/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using MVC5RealWorld.Models.ViewModel;
+using MVC5RealWorld.Models.EntityManager;
+
+namespace MVC5RealWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult ManageUserPartial()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string loginName = User.Identity.Name;
+                UserManager UM = new UserManager();
+                UserDataView UDV = UM.GetUserDataView(loginName);
+                return PartialView(UDV);
+            }
+            return View();
+        }
+    }
+    }
